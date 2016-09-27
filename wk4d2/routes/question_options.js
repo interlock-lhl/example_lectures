c36074d84f6de74f29f74bb20d56dbe0caddb3c7
James Sapara <james.sapara@gmail.com>
2016-09-27 08:11:02 -0700

    minor updates for routing

---
+++ wk4d2/routes/question_options.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/question_options', function(req, res) {
+  res.sendStatus(404);
+})
+
+module.exports = router;
---
+++ wk4d2/routes/submissions.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/submissions', function(req, res) {
+  res.sendStatus(404);
+})
+
+module.exports = router;
