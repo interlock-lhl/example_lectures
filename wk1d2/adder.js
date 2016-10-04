f5c0bb79f3daad55eb0747871c16f5d7b9d42558
James Sapara <james.sapara@gmail.com>
2016-10-04 10:24:25 -0700

    more refactor for stuff in the stuff stuff

--- wk1d2/adder.js
+++ wk1d2/adder.js
@@ -1,6 +1,6 @@
 // console.log(process.argv.slice(2));
 
-var numbs = process.argv.slice(2);
+// var numbs = process.argv.slice(2);
 
 // console.log(numbs);
 
@@ -11,20 +11,34 @@ var numbs = process.argv.slice(2);
 function sum(numbers) {
   var total = 0;
   // i++ => i += 1 => i = i + 1
-  for(var i = 0; i < numbers.length; i++) {
+  // numbers.forEach(function(a,b,c) {
+  //   console.log('a:', a, 'b:', b, 'c:', c);
+  // });
+  // var i = 0;
+  // for(var number of numbers) {
+  //   console.log(number);
+  //   // i += 1;
+  // }
+  // for(var number in numbers) {
+  //   console.log(number);
+  // }
+  for(var number of numbers) {
     // console.log("index:", i);
     // console.log("value:", numbers[i]);
 
-    var number = Number(numbers[i]);
+    var numberParsed = Number(number);
     // debugger;
-    if ( isNaN(number) === false) {
-      total += number;
+    if ( isNaN(numberParsed) === false) {
+      total += numberParsed;
+    } else {
+      console.log("Stop giving me junk: ", number)
     }
   }
   return total;
 }
 
-var total = sum(numbs);
+//var total = mapToShoppingCart(data.computeStuff(session.now).json_the_gin);
+var total = sum(process.argv.slice(2));
 console.log("TOTAL:", total);
 // sum([5,6]);
 // console.log("SECOND TOTAL:", total);
