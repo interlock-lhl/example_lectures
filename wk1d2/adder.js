66cb095ed14800a75b1570c8da99be650abd6028
James Sapara <james.sapara@gmail.com>
2016-10-04 10:12:49 -0700

    refactor in to method

--- wk1d2/adder.js
+++ wk1d2/adder.js
@@ -1,22 +1,30 @@
 // console.log(process.argv.slice(2));
 
-var numbers = process.argv.slice(2);
+var numbs = process.argv.slice(2);
 
-console.log(numbers);
+// console.log(numbs);
 
-var total = 0;
+// var total = 0;
 
-console.log(total);
+// console.log(total);
 
-// i++ => i += 1 => i = i + 1
-for(var i = 0; i < numbers.length; i++) {
-  console.log("index:", i);
-  console.log("value:", numbers[i]);
+function sum(numbers) {
+  var total = 0;
+  // i++ => i += 1 => i = i + 1
+  for(var i = 0; i < numbers.length; i++) {
+    // console.log("index:", i);
+    // console.log("value:", numbers[i]);
 
-  var number = Number(numbers[i]);
-  debugger;
-  if ( isNaN(number) === false) {
-    total += number;
+    var number = Number(numbers[i]);
+    // debugger;
+    if ( isNaN(number) === false) {
+      total += number;
+    }
   }
+  return total;
 }
-console.log('total:', total);
+
+var total = sum(numbs);
+console.log("TOTAL:", total);
+// sum([5,6]);
+// console.log("SECOND TOTAL:", total);
