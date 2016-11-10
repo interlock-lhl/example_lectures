b1082a57decc94337d4390bd65ea9ce41d25a550
James Sapara <james.sapara@gmail.com>
2016-11-10 08:46:10 -0800

    more seeds1

--- wk6d4/db/seeds.rb
+++ wk6d4/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post1 = Post.create({
+  title: "Kittens!",
+  body: "Kittens are soft and friendly and awesome and kittens!",
+  published: true,
+  comments: [
+    Comment.create(who: 'Don', body: 'I AGREE!', moderated: true, accepted: true),
+    Comment.create(who: 'James', body: 'I love kittens, but they make me sneeze!', moderated: true, accepted: true)
+  ]
+})
+
+Post.create({
+  title: "How to pet a cats belly",
+  body: "You do not pet cats belly.",
+  publish: false,
+  comments: [
+    Comment.create(who: 'David', body: 'I can confirm, do not pet cats belly', moderated: true, accepted: true),
+    Comment.create(who: 'Trump', body: 'We can build a better cat! Make cats great again!', moderated: true, accepted: false)
+  ]
+})
