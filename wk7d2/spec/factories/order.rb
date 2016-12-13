edb62112d363eda41de10be3aff7ef2988462066
James Sapara <james.sapara@gmail.com>
2016-12-13 06:45:53 -0800

    nest the factory for order

--- wk7d2/spec/factories/order.rb
+++ wk7d2/spec/factories/order.rb
@@ -1,16 +1,18 @@
 FactoryGirl.define do
   factory :order do
-    
+
     total_cents { Faker::Number.between(100, 99999) }
     stripe_charge_id { Faker::Crypto.md5 }
     email { Faker::Internet.email }
 
-    transient do
-      line_item_count 2
-    end
+    factory :order_with_line_items do
+      transient do
+        line_item_count 2
+      end
 
-    after(:create) do |order, evaluator|
-       create_list(:line_item, evaluator.line_item_count, order: order)
-     end
+      after(:create) do |order, evaluator|
+        create_list(:line_item, evaluator.line_item_count, order: order)
+      end
+    end
   end
 end
